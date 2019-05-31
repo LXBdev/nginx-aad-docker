0107ed8e68bd331a6066d1b35ee2b5a4e8fcaad5
Alex <xy.hochdorf@gmx.de>
2019-05-31 17:33:47 +0200

    add proper readme.md

---
+++ run_windows.cmd
@@ -0,0 +1,7 @@
+set OAUTH2_PROXY_CLIENT_ID=00000000-0000-0000-0000-000000000000
+set OAUTH2_PROXY_AZURE_TENANT=yourtenant.onmicrosoft.com
+set OAUTH2_PROXY_CLIENT_SECRET=crypticsecretvalue
+
+docker build -t nginx-aad "."
+echo starting...
+docker-compose up
